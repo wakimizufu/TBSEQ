1c3ebfd6980b2aa3a5376cae1e7c044f586c646f
wakimizufu <89113273+wakimizufu@users.noreply.github.com>
2024-05-16 19:54:30 +0900

    matrixSW

--- testSwLed/testSwLed.ino
+++ testSwLed/testSwLed.ino
@@ -8,60 +8,23 @@
 #include <Wire.h>
 
 #include "matrixLED.h"
-
+#include "matrixSW.h"
 
 //I2Cアドレス
 const char I2C_ADDR_SW  = 0x20; //MCP27017:マトリクススイッチ
 const char I2C_ADDR_LED = 0x21; //MCP27017:LEDスイッチ
 const char I2C_ADDR_EEPROM = 0x50; //24LC256
 
-//マトリクススイッチ
-//サンプリング回数 (0~設定値-1)
-#define SW_CNT_SAMPLE 3
-//スキャン行数 (0~設定値-1)
-#define SW_MAX_ROW 8
-
-char sampleSwitch; //現在のサンプリング回数(0~設定値-1)
-char scanSwitch[SW_CNT_SAMPLE][SW_MAX_ROW]; //サンプリング結果
-char valueSwitch[SW_MAX_ROW]; //最新の値
-
-//マトリクスLED
-//LED行数 (0~設定値-1)
-//#define LED_MAX_ROW 4
-//char valueLED[LED_MAX_ROW]; //最新の値
-
 //マトリクスLED
 matrixLED cMatrixLed;
+
+//マトリクススイッチ
+Switch cSwitch;
 
 bool toggle_panelWR(void *) {
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); // toggle the LED
   return true; // repeat? true
 }
-
-
-/*
-マトリクススイッチ,マトリクスLED関連配列を初期化
-*/
-/*
-void init_PanelArray(){
-
-  char sampleSwitch = 0;
-
-  for (int i=0;i<SW_CNT_SAMPLE;i++){
-    for (int j=0;j<SW_MAX_ROW;j++){
-      scanSwitch[i][j]  = 0x00;
-    }
-  }
-
-  for (int k=0;k<SW_MAX_ROW;k++){
-    valueSwitch[k]  = 0x00;
-  }
-
-  for (int l=0;l<SW_MAX_ROW;l++){
-    valueLED[l]  = 0xFF;
-  }
-}
-*/
 
 
 
